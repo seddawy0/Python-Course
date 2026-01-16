9772111cde0f87e672c905e8e54f1051572fa451
Seddawy0 <seddawy0@gmail.com>
2026-01-17 01:26:19 +0200

    🎮 MadLibsGame: Word game

---
+++ Projects/MadLibsGame.py
@@ -0,0 +1,16 @@
+# Word game where you can create a story
+
+print("######################### Variables ###########################")
+adjective_1 = input("Enter an adjective (description): ")
+noun_1 = input("Enter a noun (person, place or thing): ")
+adjective_2 = input("Enter an adjective (description): ")
+verb_1 = input("Enter a verb 'ending with ing': ")
+adjective_3 = input("Enter an adjective (description): ")
+print("###############################################################")
+
+print("########################### Story #############################")
+print(f"Today I went to a {adjective_1} zoo.")
+print(f"In an exhibit, I saw a {noun_1}.")
+print(f"{noun_1} was {adjective_2} and {verb_1}.")
+print(f"I was {adjective_3}!")
+print("###############################################################")    
