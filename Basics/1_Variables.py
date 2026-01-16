229ea043fbf8aa2c2bab047549330e45292a5d79
Seddawy0 <seddawy0@gmail.com>
2026-01-17 00:30:51 +0200

    Variables: String, Integer, Float and Boolean

--- Basics/1_Variables.py
+++ Basics/1_Variables.py
@@ -1,2 +1,45 @@
 # Container for a value (string, integer, float, boolean)
-name = "Seddawy0"
+
+#################################################################
+#String
+fullName = "Ziad Elsedawy"
+userName = "Seddawy0"
+nationality = "Egyptian"
+email = "seddawy0@gmail.com"
+print("############################## Strings ##############################")
+print(f"Your full name is: {fullName}")
+print(f"Your username is: {userName}")
+print(f"You are a/an {nationality} citizen")
+print(f"Your email address is: {email}")
+print("#####################################################################")
+print()
+#################################################################
+#Integer
+age = 19
+money = 50
+quantity = 12
+print("############################## Integers #############################")
+print(f"You are {age} years old")
+print(f"You have ${money}")
+print(f"The pack contains of {quantity} bottle")
+print("#####################################################################")
+print()
+#################################################################
+#Float
+price = 9.99
+gpa = 3.7
+distance = 5.5
+print("############################## Float ################################")
+print(f"The price of the pizza is: ${price}")
+print(f"Your GPA is: {gpa}")
+print(f"You have moved {distance}m today!")
+print("#####################################################################")
+print()
+#################################################################
+#Boolean
+is_student = True
+is_rich = False
+print("############################## Booleans #############################")
+print(f"Are you a student?: {is_student}")
+print(f"Are you a rich man?: {is_rich}")
+print("#####################################################################")
