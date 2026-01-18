8f3ae1f41fbcdf662fe685c91eaba6cf336f8c9a
Seddawy0 <seddawy0@gmail.com>
2026-01-18 05:16:20 +0200

    Calculator v1

--- Projects/Calculator.py
+++ Projects/Calculator.py
@@ -1,3 +1,21 @@
-num1 = 5
-num2 = 15
-op = "+"
+print("######################### Calculator ###########################")
+# Calculator v1
+num1 = float(input("Enter the 1st number: "))
+operator = input("Enter an operator (+ - * /): ")
+num2 = float(input("Enter the 2nd number: "))
+print("------------------------------")
+if operator == "+":
+    result = num1 + num2
+    print(round(result, 2))
+elif operator == "-":
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == "*":
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == "/":
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print(f"{operator} is NOT a valid operator!")
+print("################################################################")
