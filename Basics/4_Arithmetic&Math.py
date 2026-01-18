ac9928ef16080fa6dec17ccb949e7294181d68ec
Seddawy0 <seddawy0@gmail.com>
2026-01-18 02:37:04 +0200

    Arithmetic & Math: Operations & Useful Functions

---
+++ Basics/4_Arithmetic&Math.py
@@ -0,0 +1,66 @@
+import math
+print("###################### Operations #######################")
+# Operations
+num = 10
+print(num)
+print("--------")
+num += 1
+print(num)
+print("--------")
+num = 10
+num -= 1
+print(num)
+print("--------")
+num = 10
+num *= 2
+print(num)
+print("--------")
+num = 10
+num /= 2
+print(num)
+print("--------")
+num = 10
+num **= 2
+print(num)
+print("--------")
+num = 10
+remainder = num % 2
+print(remainder)
+print("--------")
+print("#########################################################")
+print()
+print("################### Useful Functions ####################")
+#Useful Functions
+x = 3.14
+y = -4
+z = 5
+result = round(x) # If it is bigger than half it increases; If it is less than half it decreases.
+print(result)
+print("--------")
+result = abs(y)
+print(result)
+print("--------")
+result = pow(x, z)
+print(result)
+print("--------")
+result = max(x, y, z)
+print(result)
+print("--------")
+result = min(x, y, z)
+print(result)
+print("--------")
+print(math.pi)
+print("--------")
+print(math.e)
+print("--------")
+a = 16.4
+result = math.sqrt(a)
+print(result)
+print("--------")
+result = math.ceil(a) # Always up
+print(result)
+print("--------")
+result = math.floor(a) # Always down
+print(result)
+print("--------")
+print("#########################################################")
