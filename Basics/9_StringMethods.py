b4a8445a1eb123d1a8bf6df76fc5668048d07b49
Seddawy0 <seddawy0@gmail.com>
2026-01-19 23:58:51 +0200

    String Methods

---
+++ Basics/9_StringMethods.py
@@ -0,0 +1,13 @@
+# help(str) for all string methods
+
+print("################### String Methods ####################")
+name = "seddawy 0 "
+print(len(name))          # Counts all characters
+print(name.find("s"))     # Finds the place of an alpha
+print(name.capitalize())  # Makes the first alpha capital
+print(name.upper())       # Makes all alpha capital
+print(name.isdigit())     # Checks if all digits or not
+print(name.strip())       # Deletes the spaces in the begining and the ending
+print("#######################################################")
+
+
