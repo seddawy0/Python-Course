e71a1dada39478f98b7be136c998e558c414af1c
Seddawy0 <seddawy0@gmail.com>
2026-02-20 23:26:43 +0200

    PyQt5 GUI Intro 🖥️

---
+++ PyQt5/PyQt5_GUI_Intro.py
@@ -0,0 +1,19 @@
+print("##################### PyQt5 GUI Intro ######################")
+import sys # System
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("My First GUI") # The title on the top left of the window
+        self.setGeometry(700, 300, 500, 500) # x-axis, y-axis, width, height
+        self.setWindowIcon(QIcon("D:\\Work\\Python\\PyQt5\\icon.jpg")) # The icon on the top left
+        
+def main():
+    app = QApplication(sys.argv) # argv =  A list of command line arguments
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_()) # Window won't close unless I closed it by myself
+if __name__ == "__main__":
+    main()
+print("############################################################")
